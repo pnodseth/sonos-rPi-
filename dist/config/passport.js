b738a3b7cb0cdf764f5d7d5d573d53d1aa12d6c4
Pål Nødseth <p@nodseth.no>
2020-03-08 12:28:16 +0100

    various ts fixes

--- dist/config/passport.js
+++ dist/config/passport.js
@@ -1,20 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = __importDefault(require("mongoose"));
 var JwtStrategy = require("passport-jwt").Strategy, ExtractJwt = require("passport-jwt").ExtractJwt;
-// load up the user model
-var User = mongoose_1.default.model("User");
-var secret = process.env.DB_SECRET;
-console.log("secret: ", secret);
+var User_1 = require("../models/User");
 module.exports = function (passport) {
-    var opts = {};
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-    opts.secretOrKey = process.env.DB_SECRET;
+    var opts = {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+        secretOrKey: process.env.DB_SECRET
+    };
     passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-        User.findOne({ _id: jwt_payload._id }, function (err, user) {
+        User_1.User.findOne({ _id: jwt_payload._id }, function (err, user) {
             if (err) {
                 return done(err, false);
             }
--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
+import { model, Model } from "mongoose";
 const JwtStrategy = require("passport-jwt").Strategy,
   ExtractJwt = require("passport-jwt").ExtractJwt;
+import { User } from "../models/User";
 
-// load up the user model
-var User = mongoose.model("User");
-const secret = process.env.DB_SECRET;
-console.log("secret: ", secret);
+type jwtStrategyOptions = {
+  jwtFromRequest: string;
+  secretOrKey: string;
+};
 module.exports = function(passport) {
-  var opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-  opts.secretOrKey = process.env.DB_SECRET;
+  var opts: jwtStrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+    secretOrKey: process.env.DB_SECRET
+  };
+
   passport.use(
     new JwtStrategy(opts, function(jwt_payload, done) {
       User.findOne({ _id: jwt_payload._id }, function(err, user) {
