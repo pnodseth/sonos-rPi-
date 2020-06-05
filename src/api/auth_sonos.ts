e17d5fec980ad0c4c5f2f2477077814f3cd87e1b
Pål Nødseth <pal.nodseth@eggsdesign.com>
2020-06-05 15:28:10 +0200

    Change auth token flow

--- src/api/auth_sonos.ts
+++ src/api/auth_sonos.ts
@@ -1,96 +1,130 @@
 /* https://developer.sonos.com/reference/authorization-api/create-token/ */
 /* When user authorizes with Sonos through client app, we need to obtain accessToken and store it on user in db */
 import fetch from "node-fetch";
-import { IUser } from "../models/models.interface";
+import {IUser} from "../models/models.interface";
 
 async function createAccessTokenFromAuthCodeGrant(code: string, redirectUri: string, user: IUser) {
-  const postData = `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`;
-  console.log("postdata: ", postData);
-  try {
-    const response = await baseTokenRequest(postData);
+    const postData = `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`;
+    console.log("postdata: ", postData);
+    try {
+        const response = await baseTokenRequest(postData);
 
-    if (response.ok) {
-      let data = await response.json();
-      user.accessToken = data.access_token;
-      user.refreshToken = data.refresh_token;
-      user.accessTokenExpirationTimestamp = new Date().getTime() + data.expires_in;
+        if (response.ok) {
+            let data = await response.json();
+            user.accessToken = data.access_token;
+            user.refreshToken = data.refresh_token;
+            user.accessTokenExpirationTimestamp = new Date().getTime() + data.expires_in;
 
-      try {
-        await user.save();
-        return;
-      } catch (err) {
-        console.log("error saving user: ", err);
-      }
-    } else {
-      console.log("error : ", response);
+            try {
+                await user.save();
+                return;
+            } catch (err) {
+                console.log("error saving user: ", err);
+            }
+        } else {
+            console.log("error : ", response);
+        }
+    } catch (err) {
+        console.log("error in createAccessTokenFromAuthCodeGrant: ", err);
     }
-  } catch (err) {
-    console.log("error in createAccessTokenFromAuthCodeGrant: ", err);
-  }
 }
 
 /* Function that first checks expiry timestamp on access token stored in Db. If not expired, use access token from DB. If expired, use refresh token in db
  to get a new access token */
 async function getAccessTokenFromDBorRefreshToken(user: IUser) {
-  let now = new Date().getTime();
-  let { accessTokenExpirationTimestamp } = user;
+    let now = new Date().getTime();
+    let {accessTokenExpirationTimestamp} = user;
 
-  /* Access Token expired, get new with refresh token  */
-  if (now > accessTokenExpirationTimestamp) {
-    console.log("getting new access token from refresh token...");
-    /* User has a refresh token stored, get new access token */
-    if (user.refreshToken !== "") {
-      const postData: string = `grant_type=refresh_token&refresh_token=${user.refreshToken}`;
+    /* Access Token expired, get new with refresh token  */
+    if (now > accessTokenExpirationTimestamp) {
+        console.log("getting new access token from refresh token...");
+        /* User has a refresh token stored, get new access token */
+        if (user.refreshToken !== "") {
+            const postData: string = `grant_type=refresh_token&refresh_token=${user.refreshToken}`;
 
-      try {
-        const response = await baseTokenRequest(postData);
+            try {
+                const response = await baseTokenRequest(postData);
 
-        if (response.ok) {
-          let data = await response.json();
-          user.accessToken = data.access_token;
-          user.refreshToken = data.refresh_token;
-          user.accessTokenExpirationTimestamp = new Date().getTime() + data.expires_in;
+                if (response.ok) {
+                    let data = await response.json();
+                    user.accessToken = data.access_token;
+                    user.refreshToken = data.refresh_token;
+                    user.accessTokenExpirationTimestamp = new Date().getTime() + data.expires_in;
 
-          try {
-            await user.save();
-            return {
-              accessToken: data.access_token,
-              refreshToken: data.refresh_token
-            };
-          } catch (err) {
-            console.log("error saving access token on user: ", err);
-          }
+                    try {
+                        await user.save();
+                        return {
+                            accessToken: data.access_token,
+                            refreshToken: data.refresh_token
+                        };
+                    } catch (err) {
+                        console.log("error saving access token on user: ", err);
+                    }
+                }
+            } catch (err) {
+                console.log("Error getting ");
+            }
+            /* User doesn't have a refresh token stored, somehow inform user */
+        } else {
+            console.log("!! User doesn't have a refresh token stored. User needs to re-authorize Sonos through client app");
         }
-      } catch (err) {
-        console.log("Error getting ");
-      }
-      /* User doesn't have a refresh token stored, somehow inform user */
-    } else {
-      console.log("!! User doesn't have a refresh token stored. User needs to re-authorize Sonos through client app");
-    }
 
-    /* Access token still valid, use it. */
-  } else {
-    console.log("getting access token stored in DB");
-    return { accessToken: user.accessToken, refreshToken: user.refreshToken };
-  }
+        /* Access token still valid, use it. */
+    } else {
+        console.log("getting access token stored in DB");
+        return {accessToken: user.accessToken, refreshToken: user.refreshToken};
+    }
 }
 
 async function baseTokenRequest(postData: any) {
-  const url = "https://api.sonos.com/login/v3/oauth/access";
-  const headers = {
-    "Content-type": "application/x-www-form-urlencoded",
-    Authorization: "Basic M2EwNmRhMjEtNjg5Zi00Mjg2LWFmOWItNTMwNDc0OTI2ZjI3OmIxZDc5ZGMyLTU5NTEtNDIyMy1hNzQ3LWNlNzdiMDE0ZTBmNQ=="
-  };
-  return fetch(url, {
-    headers,
-    method: "POST",
-    body: postData
-  });
+    const url = "https://api.sonos.com/login/v3/oauth/access";
+    const headers = {
+        "Content-type": "application/x-www-form-urlencoded",
+        Authorization: "Basic M2EwNmRhMjEtNjg5Zi00Mjg2LWFmOWItNTMwNDc0OTI2ZjI3OmIxZDc5ZGMyLTU5NTEtNDIyMy1hNzQ3LWNlNzdiMDE0ZTBmNQ=="
+    };
+    return fetch(url, {
+        headers,
+        method: "POST",
+        body: postData
+    });
 }
 
+
+async function getNewAccessTokenFromRefreshToken(user: IUser) {
+    const postData: string = `grant_type=refresh_token&refresh_token=${user.refreshToken}`;
+
+    try {
+        const response = await baseTokenRequest(postData);
+
+        if (response.ok) {
+            let {access_token, refresh_token} = await response.json();
+
+            user.accessToken = access_token;
+            user.refreshToken = refresh_token;
+
+
+            try {
+                await user.save();
+                return {
+                    accessToken: access_token,
+                    refreshToken: refresh_token
+                };
+            } catch (err) {
+                console.log("error saving access token on user: ", err);
+            }
+        } else {
+            console.log("getNewAccessTokenFromRefreshToken -> response not ok: ", response.statusText)
+        }
+    } catch (err) {
+        console.log("Error getting ");
+    }
+}
+
+
 module.exports = {
-  baseTokenRequest,
-  createAccessTokenFromAuthCodeGrant,
-  getAccessTokenFromDBorRefreshToken
+    baseTokenRequest,
+    createAccessTokenFromAuthCodeGrant,
+    getAccessTokenFromDBorRefreshToken,
+    getNewAccessTokenFromRefreshToken
+
 };
--- src/api/sonos.ts
+++ src/api/sonos.ts
@@ -1,64 +1,126 @@
+
 const fetch = require("node-fetch");
-const { getAccessTokenFromDBorRefreshToken } = require("./auth_sonos");
+const {getAccessTokenFromDBorRefreshToken, getNewAccessTokenFromRefreshToken} = require("./auth_sonos");
 
 export async function togglePlayPause(room: string, command: string, user) {
-  const endpoint = `groups/${room}/playback/${command}`;
-  const body = {};
-  try {
-    const response: Response = await baseSonosApiRequest({
-      endpoint,
-      method: "POST",
-      body: JSON.stringify(body),
-      user
-    });
-    return response;
-  } catch (err) {
-    console.log(err);
-  }
+    const endpoint = `groups/${room}/playback/${command}`;
+    const body = {};
+    try {
+        const response: Response = await baseSonosApiRequest({
+            endpoint,
+            method: "POST",
+            body: JSON.stringify(body),
+            user
+        });
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 export async function startPlayback(room: string, playlist: string, user) {
-  const endpoint: string = `groups/${room}/playlists`;
-  const body = {
-    playlistId: playlist.toString(),
-    playOnCompletion: true
-  };
-  try {
-    const response: Response = await baseSonosApiRequest({
-      endpoint,
-      method: "POST",
-      body: JSON.stringify(body),
-      user
-    });
-    return response;
-  } catch (err) {
-    console.log("error", err);
-  }
+    const endpoint: string = `groups/${room}/playlists`;
+    const body = {
+        playlistId: playlist.toString(),
+        playOnCompletion: true
+    };
+    try {
+        const response: Response = await baseSonosApiRequest({
+            endpoint,
+            method: "POST",
+            body: JSON.stringify(body),
+            user
+        });
+        return response;
+    } catch (err) {
+        console.log("error", err);
+    }
 }
 
-export async function baseSonosApiRequest({ endpoint, method, body, user }) {
-  try {
+export async function baseSonosApiRequest({endpoint, method, body, user}) {
     let url: string = `https://api.ws.sonos.com/control/api/v1/${endpoint}`;
-    const { accessToken }: { accessToken: string } = await getAccessTokenFromDBorRefreshToken(user);
+    try {
+        const {accessToken}: { accessToken: string } = await getAccessTokenFromDBorRefreshToken(user);
 
+        const headers = {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            Host: "api.ws.sonos.com"
+        };
+
+        return fetch(url, {
+            headers,
+            method,
+            body
+        });
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+
+/*
+* New test functions
+* */
+
+export async function sonosApiRequest({endpoint, method, body, user}) {
+    let url: string = `https://api.ws.sonos.com/control/api/v1/${endpoint}`;
     const headers = {
-      "Content-type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-      Host: "api.ws.sonos.com"
+        "Content-type": "application/json",
+        Authorization: "",
+        Host: "api.ws.sonos.com"
     };
 
-    return fetch(url, {
-      headers,
-      method,
-      body
-    });
-  } catch (err) {
-    throw new Error(err);
-  }
+    const {accessToken, refreshToken} = user
+
+    // User has tokens in redis, try api request
+    if (accessToken && refreshToken) {
+        headers.Authorization = `Bearer ${accessToken}`
+
+        const response =  fetch(url, {
+            headers,
+            method,
+            body
+        });
+
+        //Response ok, playback started
+        if (response.ok ) {
+            console.log(`Sonos API request to endpoint ${endpoint} success`)
+            return;
+
+        // Response not ok
+        } else {
+            // Token expired, get new token
+            if (response.status === 401) {
+                const {accessToken } = getNewAccessTokenFromRefreshToken(user)
+                headers.Authorization = `Bearer ${accessToken}`
+
+                const response =  fetch(url, {
+                    headers,
+                    method,
+                    body
+                });
+
+                // Request successful
+                if (response.ok) {
+                    console.log(`Sonos API request to endpoint ${endpoint} success`)
+                    return
+
+                // Request not successful
+                } else {
+                    console.log(`Sonos API request to endpoint ${endpoint} failed with status ${response.status}: ${response.statusText}`)
+                }
+            }
+        }
+    }
 }
 
+
+
 module.exports = {
-  startPlayback,
-  togglePlayPause,
-  baseSonosApiRequest
+    startPlayback,
+    togglePlayPause,
+    baseSonosApiRequest
 };
+
+
