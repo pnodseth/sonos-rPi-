b8c02a3285699301ff0d581de7cc5d79b7244e6d
Pål Nødseth <pal.nodseth@eggsdesign.com>
2021-02-16 09:22:10 +0100

    update db endpoint

--- src/mqttHandler.ts
+++ src/mqttHandler.ts
@@ -10,11 +10,8 @@ const mqttClient = mqtt.connect(process.env.MQTT_URL, {
 });
 
 export default function mqttHandler() {
-
-
   mqttClient.on("connect", function () {
     console.log("mqtt connected");
-
 
     mqttClient.subscribe("device/rfid/loadPlaylist", function () {
       /* ERROR HANDLING */
@@ -28,9 +25,9 @@ export default function mqttHandler() {
       /* ERROR HANDLING */
     });
 
-    mqttClient.subscribe("device/pong", function() {
+    mqttClient.subscribe("device/pong", function () {
       /*ERROR HANDLING*/
-    })
+    });
   });
 
   mqttClient.on("error", function (err) {
@@ -38,70 +35,73 @@ export default function mqttHandler() {
   });
 
   mqttClient.on("message", function (topic: string, message: string) {
-     const {deviceId, rfid, isDev, command} = JSON.parse(message)
-
+    const { deviceId, rfid, isDev, command } = JSON.parse(message);
 
     // If we are in dev mode, we can add isDev to mqtt messages to prevent triggering production server
-      if (process.env.NODE_ENV === "PROD" && isDev) {
-        return;
-      } else {
+    if (process.env.NODE_ENV === "PROD" && isDev) {
+      return;
+    } else {
+      Device.findOne({ deviceId }, (err, device) => {
+        if (err || !device) {
+          console.log(`mqttHandler -> ${topic} -> no device with deviceId ${deviceId} found`);
+        } else {
+          console.log(`device found for id ${deviceId}:`);
 
-
-        Device.findOne({deviceId},(err, device) => {
-          if (err || !device) {
-            console.log(`mqttHandler -> ${topic} -> no device with deviceId ${deviceId} found`);
-          } else {
-            console.log(`device found for id ${deviceId}:`);
-            switch (topic) {
-              case "device/rfid/playback":
-                User.findById(device?.user._id)
-                  .populate("rfidChips", " -__v -user")
-                  .populate("devices")
-                  .exec(async (err: Error, user) => {
-                    if (err) {
-                      console.log("mqttHandler -> error finding user : ", device?.user._id);
-                    }
-                    if (!user) {
-                      console.log("mqttHandler -> couldn't find user with userId: ", device?.user._id);
-                      //TODO: Send mqtt response back to blink LEDS or something
+          switch (topic) {
+            //
+            case "device/rfid/playback":
+              //
+              User.findById(device?.user._id)
+                .populate("rfidChips", " -__v -user")
+                .populate("devices")
+                .exec(async (err: Error, user) => {
+                  //
+                  if (err) {
+                    console.log("mqttHandler -> error finding user : ", device?.user._id);
+                    return;
+                  }
+                  //
+                  if (!user) {
+                    console.log("mqttHandler -> couldn't find user with userId: ", device?.user._id);
+                    return;
+                    //TODO: Send mqtt response back to blink LEDS or something
+                  }
+                  //
+                  if (command === "play" && user.rfidIsRegistering) {
+                    await createChip(user, rfid);
+                    //
+                    // start playing
+                  } else {
+                    if (!device?.sonosGroupId) {
+                      console.log(`No sonos speaker assigned to device ${device?.deviceId}`);
                     } else {
-
-                      if (command === "play" && user.rfidIsRegistering) {
-                        await createChip(user, rfid);
-                      } else {
-                        if (!device?.sonosGroupId) {
-                          console.log(`No sonos speaker assigned to device ${device?.deviceId}`);
-                        } else {
-                          handleSonosCommands(device, command,rfid,  user);
-                        }
-                      }
+                      handleSonosCommands(device, command, rfid, user);
                     }
-                  });
-                break;
+                  }
+                });
+              break;
 
-              case "device/setdevice":
-                handleSetDevice(device?.user._id, deviceId);
-                break;
+            case "device/setdevice":
+              handleSetDevice(device?.user._id, deviceId);
+              break;
 
-              case "device/pong":
+            case "device/pong":
 
-                //handleSaveDevicePong(message.toString())
+            //handleSaveDevicePong(message.toString())
 
-              default:
-                break;
-            }
-
-
+            default:
+              break;
           }
-        })
+        }
+      });
 
-        // message is Buffer
-      }
+      // message is Buffer
+    }
   });
-  return mqttClient
+  return mqttClient;
 }
 
 export function devicePing(deviceId: string): any {
   console.log(`Sending ping to device: ${deviceId}`);
-  mqttClient.publish(`todevice/${deviceId}`, "ping")
+  mqttClient.publish(`todevice/${deviceId}`, "ping");
 }
--- src/routes/deviceRoutes.ts
+++ src/routes/deviceRoutes.ts
@@ -8,16 +8,14 @@ import { data, updateUuidList } from "../helpers/deviceUuids";
 
 const router = express.Router();
 
-
 /* GET Devices*/
-router.get("/", passport.authenticate("jwt", { session: false }), function(req, res) {
+router.get("/", passport.authenticate("jwt", { session: false }), function (req, res) {
   const token: string = getToken(req.headers);
   if (token) {
     Device.find({ user: req.user._id })
       .populate("user")
       .exec((err: Error, devices: IDevice[]) => {
         if (err) {
-
           console.log("error finding devices: ", err);
           res.status(404).send();
         } else {
@@ -29,26 +27,22 @@ router.get("/", passport.authenticate("jwt", { session: false }), function(req, 
   }
 });
 
-
 /* GET single Device*/
-router.get("/:deviceId", passport.authenticate("jwt", { session: false }), function(req, res) {
+router.get("/:deviceId", passport.authenticate("jwt", { session: false }), function (req, res) {
   const token: string = getToken(req.headers);
   if (token) {
-    Device.findOne({ user: req.user._id, deviceId: req.params.deviceId })
-      .exec((err: Error, device: IDevice) => {
-        if (err) {
-          console.log("error finding device: ", err);
-          res.status(404).send(err);
-        } else {
-          res.json(device);
-        }
-      });
-
+    Device.findOne({ user: req.user._id, deviceId: req.params.deviceId }).exec((err: Error, device: IDevice) => {
+      if (err) {
+        console.log("error finding device: ", err);
+        res.status(404).send(err);
+      } else {
+        res.json(device);
+      }
+    });
   } else {
     return res.status(403).send({ success: false, msg: "Unauthorized." });
   }
 });
-
 
 /* Create new Device*/
 router.post("/", passport.authenticate("jwt", { session: false }), (req, res) => {
@@ -57,46 +51,38 @@ router.post("/", passport.authenticate("jwt", { session: false }), (req, res) =>
   if (token) {
     const { deviceId } = req.body;
 
-    Device.findOne({ deviceId })
-      .exec(async (err: Error, device: IDevice) => {
+    Device.findOne({ deviceId }).exec(async (err: Error, device: IDevice) => {
+      if (err) {
+        res.status(500).send();
+      } else {
+        // Device with that ID already exists
+        if (device) {
+          res.status(409).send();
 
-        if (err) {
-          res.status(500).send();
+          // Create new device
         } else {
-
-          // Device with that ID already exists
-          if (device) {
-            res.status(409).send();
-
-            // Create new device
-          } else {
-            try {
+          try {
             const device = await createDevice(deviceId, req.user);
-            res.status(201).json(device)
-            } catch(err) {
-              res.status(500).send()
-            }
+            res.status(201).json(device);
+          } catch (err) {
+            res.status(500).send();
           }
         }
-      });
+      }
+    });
 
     // Create new device
-
-
   } else {
     return res.status(403).send({ success: false, msg: "Unauthorized." });
   }
 });
 
-
 /* UPDATE Device*/
-router.put("/:deviceId", passport.authenticate("jwt", { session: false }), function(req, res) {
+router.put("/:deviceId", passport.authenticate("jwt", { session: false }), function (req, res) {
   const token: string = getToken(req.headers);
   if (token) {
-
     const updatedDevice = req.body;
     console.log(typeof updatedDevice);
-
 
     Device.replaceOne({ user: req.user._id, deviceId: req.params.deviceId }, updatedDevice, (err) => {
       if (err) {
@@ -106,111 +92,98 @@ router.put("/:deviceId", passport.authenticate("jwt", { session: false }), funct
         res.json(updatedDevice);
       }
     });
-
-
   } else {
     return res.status(403).send({ success: false, msg: "Unauthorized." });
   }
 });
 
 // Verify Device ID (check that it exists in uuid file
-router.get("/:id/verify/", passport.authenticate("jwt", { session: false }), async function(req, res) {
+router.get("/:id/verify/", passport.authenticate("jwt", { session: false }), async function (req, res) {
   const token: string = getToken(req.headers);
   const { id } = req.params;
   console.log("ID:  ", id);
 
   if (token) {
-    let foundDeviceId = data.find(e => e.deviceId === id);
+    let foundDeviceId = data.find((e) => e.deviceId === id);
     console.log("found device!");
     if (foundDeviceId) {
       res.send();
-
     } else {
       return res.status(404).send();
     }
-
   } else {
     return res.status(403).send({ success: false, msg: "Unauthorized." });
   }
-
 });
 
-router.get("/:id/setuser/", passport.authenticate("jwt", { session: false }), async function(req, res) {
+router.get("/:id/setuser/", passport.authenticate("jwt", { session: false }), async function (req, res) {
   const token: string = getToken(req.headers);
   const { id } = req.params;
   if (token) {
     const uuidList = data;
-    let foundDeviceId = uuidList.find(e => e.deviceId === id);
+    let foundDeviceId = uuidList.find((e) => e.deviceId === id);
     console.log("data:", uuidList);
 
     if (foundDeviceId) {
       foundDeviceId.assignedUserId = req.user._id;
       console.log("Updated list: ", uuidList);
       await updateUuidList(uuidList);
-
     } else {
       return res.status(404).send();
     }
     res.send();
-
   } else {
     return res.status(403).send({ success: false, msg: "Unauthorized." });
   }
-
 });
 
-
 /* Assign device with sonos group */
-router.post(
-  "/:id/assign",
-  passport.authenticate("jwt", { session: false }),
-  function(req, res) {
-    const token: string = getToken(req.headers);
+router.post("/:id/assign", passport.authenticate("jwt", { session: false }), function (req, res) {
+  const token: string = getToken(req.headers);
 
-    console.log("correct route");
+  console.log("correct route");
 
-    if (token) {
+  if (token) {
+    const {
+      sonosGroupId,
+      sonosGroupIdParsed,
+      sonosHouseholdId,
+    }: { sonosGroupId: string; sonosHouseholdId: string; sonosGroupIdParsed: string } = req.body;
 
-      const { sonosGroupId, sonosGroupIdParsed, sonosHouseholdId }: { sonosGroupId: string, sonosHouseholdId: string, sonosGroupIdParsed: string } = req.body;
-
-      Device.findById(req.params.id).exec(async (err: Error, device: IDevice) => {
-        if (err) {
-          console.log("error finding device: ", err);
-          res.send(err);
-        } else {
-          device.sonosGroupId = sonosGroupId;
-          device.sonosHouseholdId = sonosHouseholdId;
-          device.sonosGroupIdParsed = sonosGroupIdParsed;
-          await device.save(function(err: Error) {
-            if (err) {
-              console.log("TCL: err", err);
-              res.status(500).send();
-            }
-            res.json(device);
-          });
-        }
-      });
-    } else {
-      return res.status(403).send({ success: false, msg: "Unauthorized." });
-    }
+    Device.findById(req.params.id).exec(async (err: Error, device: IDevice) => {
+      if (err) {
+        console.log("error finding device: ", err);
+        res.send(err);
+      } else {
+        device.sonosGroupId = sonosGroupId;
+        device.sonosHouseholdId = sonosHouseholdId;
+        device.sonosGroupIdParsed = sonosGroupIdParsed;
+        await device.save(function (err: Error) {
+          if (err) {
+            console.log("TCL: err", err);
+            res.status(500).send();
+          }
+          res.json(device);
+        });
+      }
+    });
+  } else {
+    return res.status(403).send({ success: false, msg: "Unauthorized." });
   }
-);
-
+});
 
 /* PING device*/
-router.get("/:deviceId/ping", passport.authenticate("jwt", { session: false }), function(req, res) {
+router.get("/:deviceId/ping", passport.authenticate("jwt", { session: false }), function (req, res) {
   const token: string = getToken(req.headers);
   if (token) {
     devicePing(req.params.deviceId);
     res.send();
-
   } else {
     return res.status(403).send({ success: false, msg: "Unauthorized." });
   }
 });
 
-
-function createDevice(deviceId:string, user:IUser) {
+function createDevice(deviceId: string, user: IUser) {
   return new Promise((res, rej) => {
     if (process.env.NODE_ENV === "PROD") {
       //todo: Add deviceId and userId to redis, for faster lookup
@@ -219,30 +192,25 @@ function createDevice(deviceId:string, user:IUser) {
 
     const device = new Device({
       deviceId,
-      user: user._id
+      user: user._id,
     });
 
     device.save((err) => {
       if (err) {
         //res.status(500).send();
-        rej()
+        rej();
       } else {
         user.devices.push(device._id);
         user.save((err) => {
           if (err) {
-            rej()
+            rej();
           } else {
-            res(device)
-
+            res(device);
           }
         });
       }
     });
-
-  })
-
+  });
 }
-
-
 
 module.exports = router;
